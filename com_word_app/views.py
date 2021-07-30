eb544f4f8a02965873f01545b6bb369ffaa7e041
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-07-30 20:07:45 +0900

    Add nltk.txt

--- com_word_app/views.py
+++ com_word_app/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework import  views, status
+from rest_framework.response import Response
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import nltk
+import wikipedia
+import re
+class SentenceAnalyzeView(views.APIView):
 
-# Create your views here.
+    def post(self, request, *args, **kwargs):
+        sentence = request.data["sentence"]
+        sentence = word_tokenize(sentence)
+        '''
+        # ノイズ除去
+        for sw in StopWord.objects.all():
+            remove_text = '\s' + sw.word + '\s'
+            sentence = re.sub(remove_text, ' ', sentence)
+        sentence = word_tokenize(sentence)
+        fd = nltk.FreqDist(sentence)
+        fd = fd.most_common(10)
+        result = {}
+        for word_and_count in fd:
+            word = word_and_count[0]
+            count = word_and_count[1]
+            wiki_url = ""
+            search_response = wikipedia.search(word)
+            if search_response:
+                print(word)
+                print(search_response[0])
+                try:
+                    wiki_page = wikipedia.page(search_response[0])
+                    wiki_url = wiki_page.url
+                except wikipedia.DisambiguationError as e:
+                    print(e)
+                except wikipedia.PageError as e:
+                    # エラーページが何か，出さないと
+                    print(e)
+        
+            result[word] = [wiki_url, count]
+        '''
+        result = {}
+        result["abc"] = ["aa", 1]
+        print(result)
+        res_list = {
+            'result': result
+        }
+        
+        return Response(res_list, status=status.HTTP_200_OK)
