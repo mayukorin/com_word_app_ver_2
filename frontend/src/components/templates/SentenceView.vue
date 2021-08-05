fb1b877a4869d74b2ae59cde8006eee6df19efc0
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-08-05 14:22:02 +0900

    Modify v-icon position

--- frontend/src/components/templates/SentenceView.vue
+++ frontend/src/components/templates/SentenceView.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="sentence-view">
-  <div class="ma-5">
-    <div v-for="ex in explain" :key="ex.heading">
-      <v-container>
-        <v-row>
-          <v-col cols="1" class="text-right">
-            <v-icon>mdi-play</v-icon>
-          </v-col>
-          <v-col cols="11">
+    <div class="ma-5">
+      <v-container class="my-5">
+        <v-row v-for="ex in explain" :key="ex.heading">
+          <v-col cols="12"> 
             <div>
+              <v-icon>mdi-play</v-icon>
               {{ ex.heading }}
             </div>
             <v-subheader>
@@ -16,19 +13,16 @@
             </v-subheader>
           </v-col>
         </v-row>
+        <v-row>
+          <v-col cols="12" md="6">
+            <SentenceCard />
+          </v-col>
+          <v-col cols="12" md="6">
+            <WordGraphFrame />
+          </v-col>
+        </v-row>
       </v-container>
     </div>
-  </div>
-    <v-container class="my-5">
-      <v-row>
-        <v-col cols="12" md="6">
-          <SentenceCard />
-        </v-col>
-        <v-col cols="12" md="6">
-          <WordGraphFrame />
-        </v-col>
-      </v-row>
-    </v-container>
   </div>
 </template>
 <script>
@@ -43,7 +37,7 @@ export default {
   data() {
     return {
       explain: [
-        {heading: "頻出単語が分かる", detail: "頻出単語とその出現回数が棒グラフに最大10個まで表示"},
+        {heading: "頻出単語が分かる", detail: "フォームに文章を入力すれば，頻出単語とその出現回数が棒グラフに最大10個まで表示"},
         {heading: "頻出単語を調べる", detail: "棒グラフをクリックすれば，その単語に関連するwikipediaへ移動"},
         {heading: "こういう時に便利", detail: "論文を読む，参考書を読む時など..."},
       ]
